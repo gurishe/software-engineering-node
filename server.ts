db7ccbfc221daabb1f500bc8d663695fc95ec69f
Ernie Gurish <gurish.e@northeastern.edu>
2022-09-26 21:48:12 -0400

    Update mongoDB server name

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ const cors = require('cors')
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect('mongodb://localhost:27017');
+mongoose.connect('mongodb://localhost:27017/cs5500');
 
 const tuitDao = new TuitDao();
 const tuitController = new TuitController(app, tuitDao);
