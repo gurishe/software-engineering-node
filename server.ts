6312f138d5fd4cdaaefe20e1d6a5ef3321784d1d
Ernie Gurish <gurish.e@northeastern.edu>
2022-10-18 22:23:39 -0400

    testing EB

--- server.ts
+++ server.ts
@@ -15,13 +15,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/fsd');
+mongoose.connect('mongodb+srv://cs5500-eg:Zhbds2123@cs5500.rmrbkxw.mongodb.net/tuiter?retryWrites=true&w=majority');
 
 const userDao = new UserDao();
 const userController = new UserController(app, userDao);
 
 const tuitDao = TuitDao.getInstance();
 const tuitController = TuitController.getInstance(app, tuitDao);
+
+const welcome = (req: Request, res: Response) => res.send('Welcome to Foundations of Software Engineering!');
+app.get('/', welcome);
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
