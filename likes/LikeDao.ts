1174fe9612c85dc15765ddfe9f934768afa38d39
Ernie Gurish <gurish.e@northeastern.edu>
2022-11-20 09:41:22 -0500

    Modified LikeDao interface and implementation to work with isDislike flag

--- likes/LikeDao.ts
+++ likes/LikeDao.ts
@@ -43,7 +43,27 @@ export default class LikeDao implements LikeDaoI {
      */
     async findTuitsUserLiked(uid: string): Promise<Tuit[]> {
         const model = await likeModel
-            .find({likedBy: uid})
+            .find({likedBy: uid, isDislike: false})
+            .populate('tuit')
+            .populate('likedBy')
+            .exec();
+        return model.map(tuitModel =>
+            new Tuit(
+                tuitModel?.tuit?._id.toString() ?? '',
+                tuitModel?.tuit?.tuit ?? '',
+                new Date(tuitModel?.likedTuit?.postedOn ?? (new Date()))
+            )
+        );
+    }
+
+    /**
+     * Find all Tuits disliked by a specific User
+     * @param {string} uid The primary ID of the User
+     * @return {Tuit[]} An array of Tuits disliked by the User
+     */
+    async findTuitsUserDisliked(uid: string): Promise<Tuit[]> {
+        const model = await likeModel
+            .find({likedBy: uid, isDislike: true})
             .populate('tuit')
             .populate('likedBy')
             .exec();
@@ -63,7 +83,27 @@ export default class LikeDao implements LikeDaoI {
      */
     async findUsersThatLikedTuit(tid: string): Promise<User[]> {
         const model = await likeModel
-            .find({tuit: tid})
+            .find({tuit: tid, isDislike: false})
+            .populate('tuit')
+            .populate('likedBy')
+            .exec();
+        return model.map(user =>
+            new User(
+                user?.likedBy?._id.toString() ?? '',
+                user?.likedBy?.username ?? '',
+                user?.likedBy?.password ?? ''
+            )
+        );
+    }
+
+    /**
+     * Find all Users that disliked a specific Tuit
+     * @param {string} tid The unique ID of the Tuit
+     * @return {User[]} An array of Users who have disliked the Tuit
+     */
+    async findUsersThatDislikedTuit(tid: string): Promise<User[]> {
+        const model = await likeModel
+            .find({tuit: tid, isDislike: true})
             .populate('tuit')
             .populate('likedBy')
             .exec();
@@ -83,38 +123,61 @@ export default class LikeDao implements LikeDaoI {
      * @return {Like} The found Like object or an empty Like object if it is not found
      */
     findUserLikesTuit = async (uid: string, tid: string) => {
-        const like = await likeModel.findOne({tuit: tid, likedBy: uid});
+        const like = await likeModel
+            .findOne({tuit: tid, likedBy: uid});
         return new Like(
             like?._id.toString() ?? '',
             like?.likedBy ?? '',
-            like?.tuit ?? ''
-        )
+            like?.tuit ?? '',
+            like?.isDislike ?? false
+        );
     }
 
     /**
-     * Find the number of Likes on a given Tuit
+     * Find the number of Likes on a given Tuit (with isDislike flag as false)
      * @param tid The primary ID of the Tuit
      * @return {number} The number of Likes on the Tuit
      */
     async findTuitLikesCount(tid: string): Promise<any> {
-        return likeModel
-            .find({tuit: tid})
+         const likes = await likeModel
+            .find({tuit: tid, isDislike: false})
             .count();
+         const dislikes = await likeModel
+             .find({tuit:tid, isDislike: true})
+             .count();
+        return likes - dislikes;
+    }
+
+    /**
+     * Find the number of Likes with the isDislike flag true on a given Tuit
+     * @param tid The primary ID of the Tuit
+     * @return {number} The number of dislikes on the Tuit
+     */
+    async findTuitDislikesCount(tid: string): Promise<any> {
+        const likes = await likeModel
+            .find({tuit: tid, isDislike: false})
+            .count();
+        const dislikes = await likeModel
+            .find({tuit:tid, isDislike: true})
+            .count();
+        return dislikes - likes;
     }
 
     /**
      * Creates and returns a new Like
      * @param {string} uid The primary ID of the User liking the Tuit
      * @param {string} tid The primary ID of the Tuit being liked by the User
+     * @param {boolean} isDislike Flag indicating if this Like should be treated as a dislike
      * @return {Like} The newly created Like object
      */
-    async userLikesTuit(uid: string, tid: string): Promise<Like> {
+    async userLikesTuit(uid: string, tid: string, isDislike: boolean = false): Promise<Like> {
         const model = await likeModel
-            .create({tuit: tid, likedBy: uid});
+            .create({tuit: tid, likedBy: uid, isDislike: isDislike});
         return new Like(
             model?._id.toString() ?? '',
             model?.tuit?.toString() ?? '',
-            model?.likedBy?.toString() ?? ''
+            model?.likedBy?.toString() ?? '',
+            model?.isDislike ?? false
         );
     }
 
@@ -125,5 +188,17 @@ export default class LikeDao implements LikeDaoI {
      */
     async userUnlikesTuit(uid: string, tid: string): Promise<any> {
         return likeModel.deleteOne({tuit: tid, likedBy: uid});
+    }
+
+    /**
+     * Sets the isDislike flag on the given Like record
+     * @param {string} lid The primary ID of the Like record
+     * @param {boolean} isDislike The boolean value that the isDislike flag should be changed to
+     */
+    async toggleIsDislike(lid: string, isDislike: boolean): Promise<any> {
+        return likeModel.updateOne(
+            {_id: lid},
+            {$set: {isDislike: isDislike}}
+        );
     }
 }
--- likes/LikeDaoI.ts
+++ likes/LikeDaoI.ts
@@ -13,9 +13,13 @@ import User from "../users/User";
  */
 export default interface LikeDaoI {
     findTuitsUserLiked(uid: string): Promise<Tuit[]>;
+    findTuitsUserDisliked(uid: string): Promise<Tuit[]>;
     findUsersThatLikedTuit(tid: string): Promise<User[]>;
+    findUsersThatDislikedTuit(tid: string): Promise<User[]>;
     findTuitLikesCount(tid: string): Promise<any>;
+    findTuitDislikesCount(tid: string): Promise<any>
     findUserLikesTuit(uid: string, tid: string): Promise<Like>;
-    userLikesTuit(uid: string, tid: string): Promise<Like>;
+    userLikesTuit(uid: string, tid: string, isDislike: boolean): Promise<Like>;
     userUnlikesTuit(uid: string, tid: string): Promise<any>;
+    toggleIsDislike(lid: string, isDislike: boolean): Promise<any>;
 }
